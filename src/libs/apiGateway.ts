e8ce2cad5b73d1dbaec4cc9fb675e1b2565c6f60
Urbani Simone <urb.simone@gmail.com>
2021-04-19 19:31:28 +0200

    Create workspace settings and refactor files

--- src/libs/apiGateway.ts
+++ src/libs/apiGateway.ts
@@ -5,8 +5,8 @@ type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { b
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
 export const formatJSONResponse = (response: Record<string, unknown>) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
 }

e9d0a1fb110fadf44750eebab022511c06a1634f
samupd2013 <samupd2013@gmail.com>
2021-04-25 16:27:48 +0200

    created getCart, changed Cart model to CartRow

--- src/functions/carts/getCart/handler.ts
+++ src/functions/carts/getCart/handler.ts
@@ -2,12 +2,31 @@ import 'source-map-support/register';
 
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import Cart from '@dbModel/tables/cart';
-import Response from '@lamdaModel/lambdaResponse';
+import CartRow from '@dbModel/tables/cart';
+import Response from '@lamdaModel/lambdaResponse'
+import dbContext from '@dbModel/dbContext';
+import HttpStatusCodes from '@lamdaModel/httpStatusCodes';
 
-const scan: ValidatedEventAPIGatewayProxyEvent<any> = async (event) => {
-    let response: Response<Cart>;
+
+/*
+ * Remember: event.body type is the type of the instantiation of ValidatedEventAPIGatewayProxyEvent
+ * In this case event.body type is 'void' -> we have no body!
+*/
+const getCart: ValidatedEventAPIGatewayProxyEvent<void> = async (event) => {
+    let response: Response<CartRow[]>;
+    let cart: CartRow[] = new Array<CartRow>();
+    try {
+        for await (const element of dbContext.scan(CartRow)) {
+            if (event.pathParameters?.customerId == element.customerId)
+                cart.push(element);
+        }
+
+        response = Response.fromData<CartRow[]>(cart, HttpStatusCodes.OK);
+    }
+    catch (error) {
+        response = Response.fromError<CartRow[]>(error);
+    }
     return await response.toAPIGatewayProxyResult();
 }
 
-export const main = middyfy(scan);
+export const main = middyfy(getCart);

ab6a8449b67973de20a0cf0b69916a9fdddeda36
samupd2013 <samupd2013@gmail.com>
2021-04-27 18:09:00 +0200

    refactor createCheckout

--- src/functions/checkout/createCheckout/handler.ts
+++ src/functions/checkout/createCheckout/handler.ts
@@ -3,52 +3,15 @@ import 'source-map-support/register';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import Response from '@lamdaModel/lambdaResponse';
-import dbContext from '@dbModel/dbContext';
 import HttpStatusCodes from '@lamdaModel/httpStatusCodes';
 import Stripe from "stripe";
-import axios from 'axios';
-import CartRow from '@dbModel/tables/cart';
-import Product from '@dbModel/tables/product';
 import schema from '@schema/lambdaSchema/createCheckout'
-import getCart from '@carts/getCart/function'
-import getProduct from '@functions/products/getProduct/function';
+import createCheckout from '@checkout/createCheckout/function'
 
-const createCheckout: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+const createCheckoutHandler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
     let res: Response<Stripe.Response<Stripe.Checkout.Session>>;
-
-    let resultCart = await getCart('customerDefault');
-
     try {
-        let cartItems = new Array();
-        for (const i of resultCart.items) {
-
-            let productGet = await getProduct(i.productId);
-            cartItems.push({
-                price_data: {
-                    currency: "eur",
-                    product_data: {
-                        name: productGet.name,
-                        description: productGet.description,
-                    },
-                    unit_amount: productGet.price,
-                },
-                quantity: i.quantity
-            })
-        }
-
-        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-            apiVersion: "2020-08-27",
-        });
-
-        const session = await stripe.checkout.sessions.create({
-            success_url: event.body?.successUrl,
-            cancel_url: event.body?.cancelUrl,
-            payment_method_types: ['card'],
-            line_items: cartItems,
-            mode: 'payment',
-        });
-
-
+        let session = await createCheckout('customerDefault', event.body?.successUrl, event.body?.cancelUrl);
 
         res = Response.fromData<Stripe.Response<Stripe.Checkout.Session>>(session, HttpStatusCodes.OK);
     }
@@ -59,4 +22,4 @@ const createCheckout: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async 
 
 }
 
-export const main = middyfy(createCheckout);
+export const main = middyfy(createCheckoutHandler);

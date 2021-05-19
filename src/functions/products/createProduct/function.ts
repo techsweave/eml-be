8a1d12a196742a32c1caeaca758272b33097ad9a
Urbani Simone <urb.simone@gmail.com>
2021-05-19 18:03:11 +0200

    Delete all file

--- src/functions/products/createProduct/function.ts
+++
@@ -1,8 +0,0 @@
-import dbContext from '@dbModel/dbContext';
-import Product from '@dbModel/tables/product';
-
-const createProduct = async (item: Product): Promise<Product> => {
-    return dbContext.put(item);
-};
-
-export default createProduct;
